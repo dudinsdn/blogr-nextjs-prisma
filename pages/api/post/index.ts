f2a615031a68c2945566c0251181dfe5d26c235f
dudinsdn <dudinsdn@gmail.com>
2023-12-21 20:11:32 +0700

    fix authSession

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,18 +1,23 @@
-import { getSession } from "next-auth/react";
+import { getServerSession } from "next-auth/next"
+import { options as authOptions } from "../auth/[...nextauth]"
 import prisma from "../../../lib/prisma";
 
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
-export default async function handle(req, res) {
+export default async function handle(req: any, res: any) {
   const { title, content } = req.body;
 
-  const session = await getSession({ req });
+  const session = await getServerSession(req, res, authOptions);
   const result = await prisma.post.create({
     data: {
       title: title,
       content: content,
-      author: { connect: { email: session?.user?.email } },
+      author: { 
+        connect: { 
+          email: session?.user?.email 
+        } 
+      },
     },
   });
   res.json(result);
--- pages/api/publish/[id].ts
+++ pages/api/publish/[id].ts
@@ -1,7 +1,7 @@
-import prisma from "../../lib/prisma";
+import prisma from "../../../lib/prisma";
 
 // PUT /api/publish/:id
-export default async function handle(req, res) {
+export default async function handle(req: any, res: any) {
   const postId = req.query.id;
   const post = await prisma.post.update({
     where: { id: postId },
